e7a534abedfa632251b6e87d139c711d0e6a91b8
Ibrahim SOW <ibrahim.s@codeur.online>
2018-12-10 14:18:31 +0100

    start projet

---
+++ controllers/home_controller.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once('models/genre.php');
+
+
+
+// echo '<a href="/annuaire_de_films_type_allocine/films/list">films</a>';
--- index.php
+++ index.php
@@ -8,27 +8,27 @@ $controller=(count($requete)=== 1)?  "home":$requete[1];
 $action=(count($requete) < 3)? "list": $requete[2];
 $id=(count($requete) < 4)? 0: (int)$requete[3];
 
-switch ($controller) {
-        case 'realisateur':
-        require_once("controllers/realisateur_controller.php");
-        break;
+// switch ($controller) {
+//         case 'realisateur':
+//         require_once("controllers/realisateur_controller.php");
+//         break;
 
-        case 'acteur':
-        require_once("controllers/acteur_controller.php");
-        break;
+//         case 'acteur':
+//         require_once("controllers/acteur_controller.php");
+//         break;
 
-        case 'annee':
-        require_once("controllers/annee_controller.php");
-        break;
+//         case 'annee':
+//         require_once("controllers/annee_controller.php");
+//         break;
 
-        case 'acteur':
-        require_once("controllers/acteur_controller.php");
-        break; 
+//         case 'acteur':
+//         require_once("controllers/acteur_controller.php");
+//         break; 
 
-    default:
-        require_once("controllers/films_controller.php");
-        break;
-}
+//     default:
+//         require_once("controllers/films_controller.php");
+//         break;
+// }
 
 
 
