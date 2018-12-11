2ddfee9dcb4af7fc04603c4f9166d57a5f7f01a2
Ibrahim SOW <ibrahim.s@codeur.online>
2018-12-11 16:59:04 +0100

    page d'acceuil formulaire d'envoi de fichier

--- app.js
+++ app.js
@@ -87,3 +87,14 @@ for (let j = 0; j < showTrailler.length; j++) {
   };
 })();
 
+// uploader
+document.getElementById("filepicker").addEventListener("change", function(event) {
+    let output = document.getElementById("listing");
+    let files = event.target.files;
+  
+    for (let i=0; i<files.length; i++) {
+      let item = document.createElement("li");
+      item.innerHTML = files[i].webkitRelativePath;
+      output.appendChild(item);
+    };
+  }, false);
