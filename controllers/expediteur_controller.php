87444aeaf273106c04ef3eca5a2563774682a6e9
Ibrahim SOW <ibrahim.s@codeur.online>
2018-12-18 16:00:49 +0100

    add on sql data

--- controllers/expediteur_controller.php
+++ controllers/expediteur_controller.php
@@ -17,23 +17,36 @@ require_once('models/expediteur.php');
 
 
 switch ($action) {
-    case 'list':
-        showList();
+    case 'showform':
+        showForm();
         break;
+    case 'sendform':
+            sendForm();
+            break;
 }
 
 
 
 
 
-function showList(){
+function showForm(){
     global $twig, $baseurl;
+    
+    $template = $twig->load('expediteur.html.twig');
+    echo $template->render( array('title'=>'expediteur', 'baseurl' => $baseurl) );
+
+}
+
+
+function sendForm(){
+    global $twig, $baseurl;
+    if (isset($_POST['envoyer'])){
     $expediteur = expediteur();
     $destinataire = destinataire();
     $fichier = fichier();
     $message = message();
     
     $template = $twig->load('expediteur.html.twig');
-    echo $template->render( array('title'=>'expediteur', 'expediteur' => $expediteur, 'destinataire' => $destinataire, 'fichier' => $fichier, 'message' => $message,  'baseurl' => $baseurl) );
-
+    echo $template->render( array('expediteur' => $expediteur, 'destinataire' => $destinataire, 'fichier' => $fichier, 'message' => $message,  'baseurl' => $baseurl) );
+    }
 }
--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 $requete = explode("/", trim($_SERVER['REQUEST_URI'], "/"));
 
 $controller=(count($requete)=== 1)?  "home":$requete[1];
-$action=(count($requete) < 3)? "list": $requete[2];
+$action=(count($requete) < 3)? "showform": $requete[2];
 $id=(count($requete) < 4)? 0: (int)$requete[3];
 
 switch ($controller) {
-        // case 'realisateur':
-        // require_once("controllers/realisateur_controller.php");
-        // break;
+        case 'expediteur':
+        require_once("controllers/expediteur_controller.php");
+        break;
 
     default:
         require_once("controllers/expediteur_controller.php");
